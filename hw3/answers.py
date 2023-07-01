93e9f6409d76517c9b30a282581438c0aae8b509
Roei Bloch <roei.bloch@cs.technion.ac.il>
2023-07-01 19:54:46 +0300

    dry of part2 except second part of last question

--- hw3/answers.py
+++ hw3/answers.py
@@ -79,6 +79,22 @@ def part2_vae_hyperparams():
 
 part2_q1 = r"""
 **Your answer:**
+The $\sigma^2$ hyperparameter influence on the weights we give to the data loss part of out loss. which is one 
+of the 2 parts in our loss function:
+
+1. The data loss - how close is the reconstructed image to the origin
+
+2. The kldiv loss - how similar is the distribution of the latent space to the target distribution (in out case- 
+standard normal distribution)
+
+This hyperparameter directly influene the behavior of the model at the end of the training process.
+
+Setting lower values of sigma will make the model "less care" about how the reconstrufted image is close to the images
+it trained on, which leads to creating new images that are quite different from the model's dataset.
+
+On the other hand, setting higher values of sigma will make model to care alot about making the reconstructed images
+similar to the images in its dataset, which will make the final model creating images that are close to the images in the 
+dataset. in such model we will not except to get special images that we haven't seen like them before.
 
 
 """
@@ -86,11 +102,33 @@ part2_q1 = r"""
 part2_q2 = r"""
 **Your answer:**
 
+1. The reconstruction part in the loss function represents how well our decoder can make an image that
+looks like the original input. the porpuse of this part in the loss function is to help the model in generating 
+new images that will bw similar to the input images during the trainig process
+
+The KLdiv part of the loss represents how similar is the learned distribution of the latent space to our target 
+distribution (which is standard normal distribution). the purpose of the KLdiv's part is to help the model to generate
+better samples from the latent space. (to make the distribution of the samples to be similar to the distribution of the 
+data. as we called the "posterior distribution")
+
+2. During training, the encoder network learns to map input data to the parameters of the latent distribution, which we 
+consider as the mean and variance. The KL loss term compares this learned distribution to our target distribution
+and calculates the dissimilarity between them. This means that the KLdiv loss influence the learning process of the latent
+space, because it affects its differentiation, and tries to make it closer to a certain distribution.
+
+3. The first significant benefit of the KLdiv part is that it constitutes as a regularization term which prevent
+    overfitting and improves the generalization.
+    Another benefit of this term is that it makes for smooth and continuous latent space. This is reached by making
+    the distribution of the latent space closer to the normal standard distribution.
 
 """
 
 part2_q3 = r"""
 **Your answer:**
+
+The evidence distribution represents the likelihood of reconstructing an image that will resemble the input image.
+By maximizing this term, we encourage the model to reconstruct images that will be as faithfully as possible to the 
+original data, which will eventually coase for a more accurate model.
 
 
 
@@ -98,6 +136,18 @@ part2_q3 = r"""
 
 part2_q4 = r"""
 **Your answer:**
+
+we model the **log** of the latent-space variance corresponding to an input, $\bb{\sigma}^2_{\bb{\alpha}}$, in
+order to achieve the following purposes
+
+1. Stability and numerical precision: Variance values can vary over a wide range, and directly modeling them may lead to numerical instability.
+Taking the logarithm of the variance helps ensure numerical stability and precision during the training process. 
+
+2. *************this is what the chat says:*********************************8
+Constraint on Positive Values: Variance values must be positive since they represent the spread or uncertainty in the latent space. However, when 
+directly modeling the variance, there is no inherent constraint to ensure positive values. By modeling the logarithm of the variance, we implicitly 
+enforce the constraint that the variance itself must be positive, as exponentiating the logarithm will always yield a positive value.
+
 
 
 """
@@ -168,13 +218,35 @@ at last layer.
 """
 
 part4_q1 = r"""
-**Your answer:**
+*Your answer:*
+BERT achieves better results than out previeous transforner, (in around 15% better accuracy).
+this is because BERT is a much larger model, trained on larger dataset. 
+***********FINISH AFTER IMPLEMENTING PART ***********
 
+these results wouldn't be the same for any "downstream" task, because in some tasks, BERT vast understanding
+might not be as usefull as specifically built model. For example, BERT isn't very good in understanding what
+something is not, as explained in this [article](https://towardsdatascience.com/bert-is-not-good-at-7b1ca64818c5)
+
+another "downstream" task that BERT might not be usefull, is when reviews in hebrew are fanatically written in english
+alphabet. in this case, BERT will perform much worst, because it relies on its english understanding. While the
+model that is trained from scratch would have performed similarily. note: embedding might effect this scenario
 
 """
 
 part4_q2 = r"""
 **Your answer:**
+In BERT model, the last linear layers are typically the classification layers. as such, we can relate to it as more 
+task specific layers. This is unlike the internal layers, such as the multy headed attention blocks, that are rather
+general language understanding layers than task specific. 
+
+Considering this, if we fine-tune the internal layers instead of the two last linear layers, the model could perform
+well, but it will be more challenging, as it would need to understand how good the review is depend on general ungerstanding
+of the review. (such as its grammar - if it's written in first/second person for example). so eventually we can rather 
+determine that this manner won't achieve better results.
+
+to conclude, fine tuning the internal layers of the model would have been less efficient than fine tuning its last linear layers,
+as we wish to train the model to perform well on a specific task and are not willing to change its general understanding
+of the language.
 
 
 """
