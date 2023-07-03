db58ee0ca12b20faa0e2a1e930bc4b77b58e6d80
Roei Bloch <roei.bloch@cs.technion.ac.il>
2023-07-03 23:03:36 +0300

    all dry parts befome comparing to nadav's

--- hw3/answers.py
+++ hw3/answers.py
@@ -38,9 +38,10 @@ def part1_generation_params():
 
 
 part1_q1 = r"""
-**Your answer:**
-The first one is practical – The entire text is very large an loading it in its entirety onto the GPU memory is might
-not be possible or is slow if possible. Similarly, the computational graph at each loss / gradients calculation and 
+**Our answer:**
+
+The first one is practical – The entire text is very large and loading it in its entirety onto the GPU memory might
+not be possible or slow if possible. Similarly, the computational graph at each loss / gradients calculation and 
 step would be very large.
 
 The second reason is that with very large text the gradients would vanish/explode between characters with big gaps,
@@ -52,10 +53,15 @@ good minimums etc.
 """
 
 part1_q2 = r"""
-**Your answer:**
-This is due to the structure of the model – the model learns weight which based on the input change what information 
+**Our answer:**
+
+Firstly, the model is given context between batches in the form of the hidden state, and so the information is preserved
+through longer text.
+
+The model is also able to learn to preserve context outside of sequence length:
+This is due to the structure of the model – the model learns weights which based on the input change what information 
 is passed across the sequence length, and so, if some context is not changed the model wouldn’t change it, no matter
-how long the sequence it is fed.
+how long the sequence it is fed.s
 
 For example in our task, if the model has some values in $h$ corresponding to which character is talking, and this 
 context won’t change until a name with capital letters would be introduced followed by “:” the model might not forget
@@ -64,35 +70,35 @@ even if the sequence length had passed.
 """
 
 part1_q3 = r"""
-**Your answer:**
-We are not shuffling the order of batch when training because the order of them is important for the hidden state 
+**Our answer:**
+
+We are not shuffling the order of batches when training because the order of them is important for the hidden state 
 variable the model keeps. Even if we don’t propagate loss through batches the model is still better trained when being 
 fed his hidden state on the last character of the last batch.
 
-The alternative (shuffling) is to make the model’s learning task simply harder by feeding it unrelated hidden 
+The alternative (shuffling) is going to make the model’s learning task simply harder by feeding it unrelated hidden 
 state at the beginning of batch. This would also hurt the long-term memory of the model since it would be incentivized 
 to forget the context quickly between batches.
 """
 
 part1_q4 = r"""
-**Your answer:**
-1.   We lower the temperature for sampling to a value below 1 because we wish to a more distinct loss for our model.
-Meaning that for prediction with the right character having the biggest score we wish to get low losses, and for
-prediction with the right character having a smaller score we won’t the loss to be larger.
+**Our answer:**
 
-      This is especially important in character predictions, where a lot of options might be decent and so we wish 
-      to have sharper losses specifically when the model is uncertain about two characters, for example “h” or “o” 
-      after the letters “ t”, both options are good but only one is the right prediction in the current context and 
-      we wish our model to learn it – changing a lot when the model is wrong, even if by a slight amount and not 
-      caring that much if the model is correct even if by a large amount.
+1. We lower the temperature for sampling to lower the variance of characters for each model prediction. When the temperature
+is lower, the model has higher chance of sampling one of the characters that fit the context in its prediction. This
+is good foe sampling because we dont want the model to randomly make "typos", we want it to actually generate text 
+nearly deterministically.
 
 2.   When the temperature is very high, we get uniform distribution of the after-softmax probabilities, regardless 
-of the scores, making the model nearly impossible to optimize, because the would be nearly constant at any score
-values, making it’s gradient nearly 0.
+of the scores. This will cause the generated text to be nearly random.
 
 3.    When the temperature is very low, we get “onehot” of the argmax score as the after-softmax probabilities, 
-no matter what the scores are. Again in a small area around the current model parameters the probability and therefor 
-the loss are nearly constants, making the gradient nearly 0 and the model nearly impossible to optimize.
+no matter what the scores are. This will introduce zero variance in the model prediction which might cause the model
+to enter loops.
+
+For example, before an actor exits, there is a large amount of spaces followed by the letter "E". 
+We can see that the model generated random amount of spaces for such lines. We might expect that when the temperature is
+extremely low, those line would be extremely long or even infinite.
 
 """
 # ==============
@@ -116,77 +122,67 @@ def part2_vae_hyperparams():
 
 
 part2_q1 = r"""
-**Your answer:**
-The $\sigma^2$ hyperparameter influence on the weights we give to the data loss part of out loss. which is one 
-of the 2 parts in our loss function:
+**Our answer:**
 
-1. The data loss - how close is the reconstructed image to the origin
+The $\sigma^2$ hyperparameter influence on the weights we give to the data loss part of our loss.
+The hyperparameter describes how much variance should the reconstructed images have around the original x.
 
-2. The kldiv loss - how similar is the distribution of the latent space to the target distribution (in out case- 
-standard normal distribution)
-
-This hyperparameter directly influene the behavior of the model at the end of the training process.
-
-Setting lower values of sigma will make the model "less care" about how the reconstrufted image is close to the images
-it trained on, which leads to creating new images that are quite different from the model's dataset.
-
-On the other hand, setting higher values of sigma will make model to care alot about making the reconstructed images
+Setting lower values of $\sigma^2$ will make model to care alot about making the reconstructed images
 similar to the images in its dataset, which will make the final model creating images that are close to the images in the 
 dataset. in such model we will not except to get special images that we haven't seen like them before.
 
-
+Setting higher values of $\sigma^2$ will make the model "care less" about how the reconstrufted image is close to the images
+it trained on, which leads to creating new images that are quite different from the model's dataset.
 """
 
 part2_q2 = r"""
-**Your answer:**
+**Our answer:**
+
 
 1. The reconstruction part in the loss function represents how well our decoder can make an image that
 looks like the original input. the porpuse of this part in the loss function is to help the model in generating 
-new images that will bw similar to the input images during the trainig process
+new images that will be similar to the input images during the trainig process
 
 The KLdiv part of the loss represents how similar is the learned distribution of the latent space to our target 
 distribution (which is standard normal distribution). the purpose of the KLdiv's part is to help the model to generate
-better samples from the latent space. (to make the distribution of the samples to be similar to the distribution of the 
-data. as we called the "posterior distribution")
+better samples from the latent space. This will allow as to sample from $\mathcal{N}(\bb{0},\bb{I})$, decode and get
+some reconstruction in the distribution $p(X)$ space.
 
-2. During training, the encoder network learns to map input data to the parameters of the latent distribution, which we 
-consider as the mean and variance. The KL loss term compares this learned distribution to our target distribution
-and calculates the dissimilarity between them. This means that the KLdiv loss influence the learning process of the latent
-space, because it affects its differentiation, and tries to make it closer to a certain distribution.
+2. The KL loss is a regularization for the learned mapping onto the latent space.
+During training, the encoder network learns to map input data to the parameters of the latent distribution, which we 
+consider as the mean and variance. The KL loss term compares this learned distribution parameter to our target distribution
+and minimizes the dissimilarity between them.
 
 3. The first significant benefit of the KLdiv part is that it constitutes as a regularization term which prevent
     overfitting and improves the generalization.
     Another benefit of this term is that it makes for smooth and continuous latent space. This is reached by making
     the distribution of the latent space closer to the normal standard distribution.
+    Laslty it makes it easier to sample new unseen representations from the distribution.
 
 """
 
 part2_q3 = r"""
-**Your answer:**
-
-The evidence distribution represents the likelihood of reconstructing an image that will resemble the input image.
-By maximizing this term, we encourage the model to reconstruct images that will be as faithfully as possible to the 
-original data, which will eventually coase for a more accurate model.
+**Our answer:**
 
 
-
+The evidence distribution represents the probabilty of reconstructing an image that will resemble the input image.
+By maximizing this term, we encourage the model to reconstruct images that will be as faithfull as possible to the 
+original data, which will eventually cause for a more accurate model.
 """
 
 part2_q4 = r"""
-**Your answer:**
+**Our answer:**
+
 
 we model the **log** of the latent-space variance corresponding to an input, $\bb{\sigma}^2_{\bb{\alpha}}$, in
 order to achieve the following purposes
 
-1. Stability and numerical precision: Variance values can vary over a wide range, and directly modeling them may lead to numerical instability.
+1. Stability and numerical precision: Variance values can vary over a wide range, spesificly of small values, 
+and directly modeling them may lead to numerical instability.
 Taking the logarithm of the variance helps ensure numerical stability and precision during the training process. 
 
-2. *************this is what the chat says:*********************************8
-Constraint on Positive Values: Variance values must be positive since they represent the spread or uncertainty in the latent space. However, when 
-directly modeling the variance, there is no inherent constraint to ensure positive values. By modeling the logarithm of the variance, we implicitly 
-enforce the constraint that the variance itself must be positive, as exponentiating the logarithm will always yield a positive value.
-
-
+2. Variance must be positive: The variance must be positive, by modeling log variance we allow are model to output
+value over $\mathbb{R}$, and then e to it's power gives as strictly positive variance values.
 
 """
 
@@ -218,7 +214,7 @@ def part3_transformer_encoder_hyperparams():
 
 
 part3_q1 = r"""
-**Your answer:**
+*Our answer:*
 
 Each encoder layer allows for represenetation of a certain token only in relation to the tokens that are
 in $sliding-window-size * 0.5$ distance from it. That means that if we stack multiple encoder layers on top of each other, 
@@ -230,7 +226,7 @@ keep increasing like so at each layer.
 Generally we can state that for $L$ layers each token representation has information from $1 + L\cdot w$ other tokens.
 
 For example, assume the sliding window size is 4, and we have a series of 20 tokens.
-After performing 1 encoder layer, the token in index 0 has context of the tokens in 
+After performing 1 encoder layer, the token at index 0 has context of the tokens in 
 index smaller/equal to 3. And 3 has context of those in indices between 1 to 5.
 So after the second encoder layer, the representation of the token at index 0 has context of the representation of the 
 token at index 3, that already encodes the information of token at index 5. So, we can say that now the representation of the
@@ -240,54 +236,53 @@ Performing this process multiple times results in a broader context in the final
 """
 
 part3_q2 = r"""
-**Your answer:**
-We propose the “random and sliding windowed attention” which works as follows:
+*Our answer:*
 
-Each time you calculate attention matrix $A\in \mathbb{R} ^{n_X n}$ , calculate it at the $w + 1$ main diagonals
-(like we implemented) and also random $n \cdot w$ indices in $A$ and calculate them.
+We propose the “RASWA - random and sliding windowed attention” which works as follows:
+
+Each time you calculate attention matrix $A\in \mathbb{R} ^{n \vectimes n}$ , calculate it at the $w + 1$ main diagonals
+(like we implemented) and also random $n \cdot w$ indices in $A$ and calculate them as well.
 
 This would propagate information throughout the sequence globally. Like in the previous question, after $L$ layers
-each representation would have for certain context of $L \cdot w$ tokens, but for each token outside it’s certain
-context, the probability of the token not being inside the context decreases exponentialy in L, the probability being
-$(1-\frac{w}{n})^{context_{L-1}}$ where $ context_{L-1}$ is the amount of representations which token is in their context 
+each representation would have for certain context of $L \cdot w$ tokens, but for each token $t$ outside it’s certain
+context, the probability of $t$ not being inside the context decreases exponentialy in $L$, the probability being
+$(1-\frac{w}{n})^{context_{L-1}}$. Where $context_{L-1}$ is the amount of representations which $t$ is in their context 
 at last layer.
-
-
 """
 
 part4_q1 = r"""
-*Your answer:*
-BERT achieves better results than out previeous transforner, (in around 15% better accuracy).
-this is because BERT is a much larger model, trained on larger dataset. 
-***********FINISH AFTER IMPLEMENTING PART ***********
+Our answer:
 
-these results wouldn't be the same for any "downstream" task, because in some tasks, BERT vast understanding
-might not be as usefull as specifically built model. For example, BERT isn't very good in understanding what
-something is not, as explained in this [article](https://towardsdatascience.com/bert-is-not-good-at-7b1ca64818c5)
+BERT achieves better results than our previeous transforner, (around 15% better accuracy).
+This happens because BERT is a much larger model, pre trained on larger dataset, and so has stronger understanding
+of the english language then what can be obtained from our small dataset, with our smaller model which also only
+uses windowed attention. 
 
-another "downstream" task that BERT might not be usefull, is when reviews in hebrew are fanatically written in english
-alphabet. in this case, BERT will perform much worst, because it relies on its english understanding. While the
-model that is trained from scratch would have performed similarily. note: embedding might effect this scenario
+These results wouldn't be the same for any "downstream" task (altough for most - they would be), 
+because in some tasks, BERT's vast understanding might not be as usefull as specifically built model.
+For example, BERT isn't very good in understanding what something is not, 
+as explained in this [article](https://towardsdatascience.com/bert-is-not-good-at-7b1ca64818c5)
+
+Another "downstream" task that BERT might not be great at, is when reviews in hebrew are fanatically written in english
+alphabet (haseret haze haia mamash garua). in this case, BERT will perform much worst, because it relies on its english understanding.
+While the model that is trained from scratch would have performed similarily. note: embedding might effect this scenario
 
 """
 
 part4_q2 = r"""
-**Your answer:**
-In BERT model, the last linear layers are typically the classification layers. as such, we can relate to it as more 
-task specific layers. This is unlike the internal layers, such as the multy headed attention blocks, that are rather
-general language understanding layers than task specific. 
+*Our answer:*
 
-Considering this, if we fine-tune the internal layers instead of the two last linear layers, the model could perform
-well, but it will be more challenging, as it would need to understand how good the review is depend on general ungerstanding
-of the review. (such as its grammar - if it's written in first/second person for example). so eventually we can rather 
-determine that this manner won't achieve better results.
+The model would perform much worse while freezing the two last linea layers and fine-tuning only two internal layers.
 
-to conclude, fine tuning the internal layers of the model would have been less efficient than fine tuning its last linear layers,
-as we wish to train the model to perform well on a specific task and are not willing to change its general understanding
-of the language.
+In BERT model, the last linear layers are the classification layers. As such, they are generally more 
+task specific layers. This is unlike the internal layers, such as the multy headed attention blocks, that are
+general language understanding layers, and should be usfull for any task. 
 
+Thus, fine-tuning two internal layers is a much harder task than doing so to the classification head. 
+First of all, the layers we are optimizing are much farther from the output, making changes in them have expolding or
+vanishing effect on the output. Secondly, the layers we are optimizing over have much less linguistic context, because they
+are after less layers of self attension, making it harder for them to learn true lingual "goodness" or "badness" of a review.
 
 """
-
 
 # ==============
